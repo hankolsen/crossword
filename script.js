66e3532566bba061866b83ff42286c8e4c1eafa6
hank <henrik@hankolsen.com>
2018-02-04 12:28:21 +0100

    Support touch events Support arrow key navigation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+/* eslint no-confusing-arrow: 0 */
 const isValidKey = key => key.match(/^[a-zåäö]{1}$/i);
-const preventKeyDown = key => key === 'Tab' || (key !== 'Backspace' && !isValidKey(key));
+const isIgnorableKey = key => key === 'Tab' || (!isValidKey(key) && key !== 'Backspace');
 
 document
   .querySelectorAll('input')
@@ -7,28 +8,44 @@ document
 
     const body = document.querySelector('body');
 
-    // Toggle across/down on double click
-    input.addEventListener('dblclick', () => body.classList.toggle('across'));
+    // Toggle across/down on click/touch in already focused input
+    ['mousedown', 'touchstart'].forEach(listeningEvent =>
+      input.addEventListener(listeningEvent, event =>
+        document.activeElement === event.target ? body.classList.toggle('across') : true));
 
     // Prevent non alfa characters from being entered
-    input.addEventListener('keydown', event => preventKeyDown(event.key) ? event.preventDefault() : true);
+    input.addEventListener('keydown', event => isIgnorableKey(event.key) ? event.preventDefault() : true);
 
     // Handle user inputs
     input.addEventListener('keyup', ({ key, shiftKey, target }) => {
+
+      // Is it an arrow key?
+      const [, arrow] = key.match(/Arrow(\w+)$/) || [];
+
       // Ignore Shift key press, all non alfa characters, unless it is tab or backspace
-      if (key === 'Shift' || (!key.match(/^[a-zåäö]{1}$/i) && key !== 'Tab' && key !== 'Backspace')) {
+      if (key === 'Shift' || (!key.match(/^[a-zåäö]{1}$/i) && key !== 'Tab' && key !== 'Backspace' && !arrow)) {
         return;
       }
 
       // By default we are moving in forward direction
       let direction = 1;
       // For SHIFT + TAB and Backspace we move backwards
-      if ((key === 'Tab' && shiftKey) || key === 'Backspace') {
+      if ((key === 'Tab' && shiftKey) || key === 'Backspace' || arrow === 'Left' || arrow === 'Up') {
         direction = -1;
       }
 
       // Are we moving across or down
-      const isAcross = body.classList.contains('across');
+      let isAcross = body.classList.contains('across');
+
+      if (arrow) {
+        if (isAcross && (arrow === 'Up' || arrow === 'Down')) {
+          isAcross = false;
+          body.classList.toggle('across');
+        } else if (!isAcross && (arrow === 'Left' || arrow === 'Right')) {
+          isAcross = true;
+          body.classList.toggle('across');
+        }
+      }
 
       // Get the coordinates for the current element
       const [currentRow, currentColumn] = target.id.match(/\d+/g);
